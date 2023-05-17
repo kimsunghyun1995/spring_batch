a41f02e5e287cdcf349ecb43244d2b5c186dbbe1
sunghyun <tjdgus2319@gmail.com>
2023-05-17 13:30:53 +0900

    scope

---
+++ test/src/main/java/com/springbatch/test/job/ScopeJobConfiguration.java
@@ -0,0 +1,60 @@
+package com.springbatch.test.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@RequiredArgsConstructor
+@Configuration
+@Slf4j
+public class ScopeJobConfiguration {
+    private JobRepository jobRepository;
+    private PlatformTransactionManager platformTransactionManager;
+    @Bean
+    public Job scopeJob() {
+        return new JobBuilder("scopeJob",jobRepository)
+                .start(scopeStep1(null))
+                .next(scopeStep2())
+                .build();
+    }
+    @Bean
+    @JobScope
+    public Step scopeStep1(@Value("#{jobParameter[requestDate]}") String requestDate) {
+        return new StepBuilder("scopeStep1",jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is scopeStep1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+    @Bean
+    @JobScope
+    public Step scopeStep2() {
+        return new StepBuilder("scopeStep2",jobRepository)
+                .tasklet(scopeStep2Tasklet(null))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet scopeStep2Tasklet(@Value("#{jobParameter[requestDate]}") String requestDate) {
+        return (contribution, chunkContext) -> {
+            log.info(">>>> This is scopeStep2");
+            log.info(">>>> requestDate ={}", requestDate);
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
